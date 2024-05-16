eed03b7d32933f7d3edd7c779053c1f2df897676
ghulam-fareed <gulamfareedtech@gmail.com>
2024-05-16 19:29:57 +0500

    temp refresh request

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { useEffect } from "react";
 import { Home } from "./pages/home/home";
+import axios from "axios";
 // import "./App.css";
 
 function App() {
+  //remove this after buying server
+  useEffect(() => {
+    axios.get("https://e-com-backend-1zsb.onrender.com");
+  });
   return (
     <>
       <Home />
